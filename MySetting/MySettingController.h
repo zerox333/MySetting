aa801c251197e0ea3871b951dcdd5418ed790717
zerox333 <lawliet0914@gmail.com>
2012-09-08 14:33:54 +0800

    Initial Commit

---
+++ MySetting/MySettingController.h
@@ -0,0 +1,23 @@
+//
+//  MySettingController.h
+//  MySetting
+//
+//  Created by ZeroX on 12-9-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Preferences/Preferences.h>
+
+@interface MySettingController : PSListController
+{
+}
+
+- (id)getValueForSpecifier:(PSSpecifier*)specifier;
+- (void)setValue:(id)value forSpecifier:(PSSpecifier*)specifier;
+- (void)followOnTwitter:(PSSpecifier*)specifier;
+- (void)visitWebSite:(PSSpecifier*)specifier;
+- (void)makeDonation:(PSSpecifier*)specifier;
+
+@end
